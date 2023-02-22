408c34d874be2acad2815d77b0b1157e97d12943
Roberth Strand <roberth.strand@amesto.no>
2023-02-22 11:08:07 +0100

    docs: example deployment
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

--- examples/main.tf
+++ examples/main.tf
@@ -1,2 +1,24 @@
-# Example, should give the user an idea about how to use this module.
-# This code is found in the examples directory.
+resource "azurerm_resource_group" "dev" {
+  name     = "rg-demo-dev"
+  location = "westeurope"
+}
+
+module "cosmosdb" {
+  source  = "amestofortytwo/cosmosdb/azurerm"
+  version = "0.1.0"
+
+  name                = "demo-dev"
+  resource_group_name = azurerm_resource_group.dev.name
+  location            = azurerm_resource_group.dev.location
+
+  consistency_policy = {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 10000
+  }
+
+  geo_location = {
+    failover_priority = 0
+    location          = "northeurope"
+  }
+}
